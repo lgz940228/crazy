af5e62ac59d4e12ff75da2523bf2fadfd493123d
刘国章 <guozhang.liu>
2019-01-16 18:27:46 +0800

    feat:*.do

---
+++ src/main/java/com/lgz/crazy/config/MyWebMvcConfigurer.java
@@ -0,0 +1,26 @@
+package com.lgz.crazy.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by lgz on 2019/1/16.
+ */
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer{
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        //开启路径后缀匹配
+        configurer.setUseRegisteredSuffixPatternMatch(true);
+    }
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean<DispatcherServlet> servletServletRegistrationBean = new ServletRegistrationBean<>(dispatcherServlet);
+        servletServletRegistrationBean.addUrlMappings("*.do");
+        return servletServletRegistrationBean;
+    }
+}
