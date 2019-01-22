90f47ce6acd244395183b484c68de4d20e43a184
刘国章 <guozhang.liu>
2019-01-22 17:42:03 +0800

    feat:页面修改

--- src/main/java/com/lgz/crazy/controller/hello.java
+++ src/main/java/com/lgz/crazy/controller/hello.java
@@ -3,6 +3,9 @@ package com.lgz.crazy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 /**
  * Created by lgz on 2019/1/16.
@@ -17,5 +20,17 @@ public class hello {
     @RequestMapping("s")
     public String success(){
         return "success";
+    }
+
+    @RequestMapping("index")
+    public String index(Map<String,String> map){
+        map.put("hello","你好");
+        return "index";
+    }
+    @RequestMapping("index1")
+    public ModelAndView index1(){
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("hello","你好");
+        return mv;
     }
 }
