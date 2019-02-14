a602e04f5069ae6bb8512a15b481897ae8e9304a
刘国章 <guozhang.liu>
2019-02-14 18:28:23 +0800

    fix:fastdfs

---
+++ src/test/java/com/lgz/crazy/fdfs/FdfsTest.java
@@ -0,0 +1,12 @@
+package com.lgz.crazy.fdfs;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by lgz on 2019/2/14.
+ */
+@RunWith(SpringRunner.class)
+public class FdfsTest {
+
+}
