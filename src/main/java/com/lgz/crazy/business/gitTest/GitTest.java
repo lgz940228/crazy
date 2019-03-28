eb0a4df2e5fdaeb221a5e9506254d149687fa1aa
刘国章 <guozhang.liu>
2019-03-28 15:47:04 +0800

    feat:theamleaf

--- src/main/java/com/lgz/crazy/business/gitTest/GitTest.java
+++ src/main/java/com/lgz/crazy/business/gitTest/GitTest.java
@@ -15,6 +15,10 @@ public class GitTest {
         System.out.println(1);
     }
     public static void sout2(){
-        System.out.println(3);
+        System.out.println(2);
+    }
+
+    public static void sout4(){
+        System.out.println(4);
     }
 }
