e858fa9ef5f7e5039337822d17e38c0285ee81d8
刘国章 <guozhang.liu>
2019-03-28 15:40:03 +0800

    gittest

---
+++ src/main/java/com/lgz/crazy/business/gitTest/GitTest.java
@@ -0,0 +1,20 @@
+package com.lgz.crazy.business.gitTest;
+
+/**
+ * Created by lgz on 2019/3/28.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        sout1();
+        sout2();
+    }
+
+
+    public static void sout1(){
+        System.out.println(1);
+    }
+    public static void sout2(){
+        System.out.println(3);
+    }
+}
